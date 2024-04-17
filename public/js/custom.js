a2085ecb0bc5d7cb309d250d95a17b61e3341eaf
Nanko <92059246+NankoYo@users.noreply.github.com>
2024-04-17 10:15:41 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,2 +1,1 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-var script=document.createElement("script");script.setAttribute("type","text/javascript");script.setAttribute("id","myhk");script.setAttribute("src","https://myhkw.cn/api/player/167490322175");script.setAttribute("key","167490322175");script.setAttribute("m","1");document.documentElement.appendChild(script);
