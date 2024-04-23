664eb4cfe6903a0efd4ba5b2763d1f6b2213e2dc
Nanko <92059246+NankoYo@users.noreply.github.com>
2024-04-23 16:52:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.nakeu.cn' },
-    { title: '状态监控', url: 'https://status.nakeu.cn' }
+    { title: '欢迎来到我的博客', url: 'https://blog.nankoyo.com' },
+    { title: '', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
