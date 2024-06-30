dacb78c3c15a114bcde61e986b841c5c862b0f7b
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-06-30 08:42:46 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -48,7 +48,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://nankoyo.com/',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.nankoyo.com',
+  HEO_INFO_CARD_URL3: 'https://www.nankoyo.com/',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
