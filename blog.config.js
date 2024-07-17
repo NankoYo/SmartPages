16d88aa22dd4fdff3ff858a961af741694be5516
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-07-18 05:13:07 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -192,11 +192,11 @@ const BLOG = {
   // Mermaid 图表CDN
   MERMAID_CDN:
     process.env.NEXT_PUBLIC_MERMAID_CDN ||
-    'https://cdn.staticfile.net/mermaid/10.2.4/mermaid.min.js', // CDN
+    'https://cdnjs.nankoyo.com/ajax/libs/mermaid/10.2.4/mermaid.min.js', // CDN
   // QRCodeCDN
   QR_CODE_CDN:
     process.env.NEXT_PUBLIC_QR_CODE_CDN ||
-    'https://cdn.staticfile.net/qrcodejs/1.0.0/qrcode.min.js',
+    'https://cdnjs.nankoyo.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js',
 
   BACKGROUND_LIGHT: '#eeeeee', // use hex value, don't forget '#' e.g #fffefc
   BACKGROUND_DARK: '#000000', // use hex value, don't forget '#'
