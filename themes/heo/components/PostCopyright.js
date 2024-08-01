93ddebc925aa98cd05607a2c2470695f8bc148ef
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-08-01 23:58:10 +0800

    Update PostCopyright.js

--- themes/heo/components/PostCopyright.js
+++ themes/heo/components/PostCopyright.js
@@ -43,44 +43,6 @@ export default function PostCopyright() {
           <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
           {locale.COMMON.COPYRIGHT_NOTICE}
         </li>
-        <div className="reward-container">
-  <button
-    className="reward-btn"
-    id="rewardBtn"
-  >
-    						 							❤ 打赏						  						
-  </button>
-  <p className="tea">
-    “觉得不错的话，给点打赏吧 ୧(๑•̀⌄•́๑)૭ ”
-  </p>
-  <div
-    className="reward-img-container"
-    id="rewardImgContainer"
-  >
-    <div className="singleImgContainer">
-      <img
-        alt="微信二维码"
-        className="reward-img"
-        id="wechatImg"
-        src="https://cdn.nankoyo.com/public/cache/blog/posts/wechat.jpg"
-      />
-      <p className="wechatPay">
-        微信支付
-      </p>
-    </div>
-    <div className="singleImgContainer">
-      <img
-        alt="支付宝二维码"
-        className="reward-img"
-        id="alipayImg"
-        src="https://cdn.nankoyo.com/public/cache/blog/posts/alipay.jpg"
-      />
-      <p className="aliPay">
-        支付宝支付
-      </p>
-    </div>
-  </div>
-</div>
       </ul>
     </section>
   )
