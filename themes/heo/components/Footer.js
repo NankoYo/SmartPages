20fca4a07d9a33b56e84fcedb2fd8a9baa9126ab
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-08-04 04:24:51 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,155 +1,44 @@
-import SocialButton from './SocialButton'
 import { siteConfig } from '@/lib/config'
+import SocialButton from './SocialButton'
 
 const Footer = ({ title }) => {
   const d = new Date()
   const currentYear = d.getFullYear()
   const since = siteConfig('SINCE')
-  const copyrightDate = parseInt(since) < currentYear? since + '-' + currentYear : currentYear
+  const copyrightDate =
+    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-        <footer
-            className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'
-        >
-
-            {/* 颜色过度区 */}
-            <div id='color-transition' className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'>
-
-            </div>
-
-            {/* 社交按钮 */}
-            <div className='w-full h-24'>
-                <SocialButton />
-            </div>
-
-            <br />
-
-            {/* 底部页面信息 */}
-  
-            <div
-            id='footer-bottom'
-            className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-            <div id='footer-bottom-left'>
-              <a href='https://blog.nankoyo.com/' className='mr-2'></a>
-              </div>
-              
-              <div id='footer-bottom-left'>
-    
-    <div className="github-badge-big">
-    <span className="badge-subject">
-      <i className="fa-solid fa-language" />
-      {' '}一言：
-    </span>
-    <span className="badge-value bg-orange">
-      <a
-        href="https://hitokoto.cn/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-      <span id="hitokoto"></span>
-      </a>
-    </span>
-      <span className="badge-subject">
-      <i className="fas fa-shield-alt" />
-      {' '}当前CDN节点: 
-    </span>
-    <span className="badge-value bg-orange">
-      <a
-        href="https://hitokoto.cn/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-      <span id="cdn"></span>
-      </a>
-    </span>
-    <span className="badge-subject">
-      <i
-        aria-hidden="true"
-        className="fa-brands fa-cloudflare"
+    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
+      {/* 颜色过度区 */}
+      <div
+        id='color-transition'
+        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
       />
-      {' '}CDN
-    </span>
-    <span className="badge-value bg-shallots">
-      <a
-        href="https://www.clouflare.com/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        CloudFlare
-      </a>
-    </span>
-    <span className="badge-subject">
-      <i className="fa-brands fa-wordpress" />
-      {' '}Powered
-    </span>
-    <span className="badge-value bg-orange">
-      <a
-        href="https://cn.wordpress.org/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        WordPress
-      </a>
-    </span>
-    <span className="badge-subject">
-      <i
-        aria-hidden="true"
-        className="fa fa-copyright"
-      />
-      Copyright{' '}
-    </span>
-    <span className="badge-value bg-red">
-      2021-2024
-      <a
-        href="https://www.nankoyo.com/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        @ NanKoYo, Inc.
-      </a>
-    </span>
-    <a
-      href="https://www.nankoyo.com/"
-      one-link-mark="yes"
-      target="_blank"
-    ></a>
-  </div>
-  <a
-    href="https://www.nankoyo.com/"
-    one-link-mark="yes"
-    target="_blank"
-  >
-    <div className="github-badge-big">
-      <span className="badge-subject">
-        <i className="fa-solid fa-clock" />
-        {' '}Running Time
-      </span>
-      <span className="badge-value bg-apricots">
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_days"
-        />
-        days
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_hours"
-        />
-        {' '}H
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_mins"
-        />
-        {' '}M
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_secs"
-        />
-        S
-      </span>
-    </div>
-    </a>
 
-          <div id='footer-bottom-right'>
+      {/* 社交按钮 */}
+      <div className='w-full h-24'>
+        <SocialButton />
+      </div>
+
+      <br />
+
+      {/* 底部页面信息 */}
+      <div
+        id='footer-bottom'
+        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+        <div id='footer-bottom-left'>
+          NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
+          {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
+          <a
+            href={siteConfig('LINK')}
+            className='underline font-bold dark:text-gray-300 '>
+            {siteConfig('AUTHOR')}
+          </a>
+          .
+        </div>
+
+        <div id='footer-bottom-right'>
           {siteConfig('BEI_AN') && (
             <>
               <i className='fas fa-shield-alt' />{' '}
@@ -158,7 +47,7 @@ const Footer = ({ title }) => {
               </a>
             </>
           )}
-        
+
           <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye' />
             <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
@@ -167,4 +56,12 @@ const Footer = ({ title }) => {
             <i className='fas fa-users' />{' '}
             <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
           </span>
-          </footer>
+
+          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
