0b5faf7fa9c412f71d5e1deea98542483baac605
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-08-04 01:42:27 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -25,144 +25,41 @@ const Footer = ({ title }) => {
             <br />
 
             {/* 底部页面信息 */}
-    <div className="github-badge-big">
-    <span className="badge-subject">
-      <i className="fa fa-id-card" />
-      {' '}作者{' '}
-    </span>
-    <span className="badge-value bg-orange">
-      <a
-        href="https://www.nankoyo.com/"
-        one-link-mark="yes"
-        target="_blank"
-        >
-        NanKoYo Team
-      </a>
-    </span>
-  </div>
-  <div className="github-badge-big">
-    <span className="badge-subject">
-      <i
-        aria-hidden="true"
-        className="fas fa-shield-alt"
-      />
-      {' '}当前CDN节点:
-    </span>
-    <span className="badge-value bg-shallots">
-      <a
-        href="https://www.cloudFlare.com/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        <span id="cdn" />
-      </a>
-    </span>
-    <span className="badge-subject">
-      <i className="fa fas-wordpress" />
-      {' '}Powered
-    </span>
-    <span className="badge-value bg-green">
-      <a
-        href="https://cn.wordpress.org/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        WordPress
-      </a>
-    </span>
-  </div>
-  <div className="github-badge-big">
-    <span className="badge-subject">
-      <i
-        aria-hidden="true"
-        className="fa fa-copyright"
-      />
-      Copyright{' '}
-    </span>
-    <span className="badge-value bg-red">
-      2022-2023
-      <a
-        href="https://blog.nankoyo.com/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        @ NanKoYo
-      </a>
-    </span>
-    <a
-      href="https://blog.nankoyo.com/"
-      one-link-mark="yes"
-      target="_blank"
-    >
-    </a>
-  </div>
-  <a
-    href="https://blog.nankoyo.com/"
-    one-link-mark="yes"
-    target="_blank"
-  >
-    <div className="github-badge-big">
-    <span className="badge-subject">
-      <i
-        aria-hidden="true"
-        className="fas fa-shield-alt"
-      />
-      一言{' '}
-    </span>
-    <span className="badge-value bg-red">
-      <a
-        href="https://blog.nankoyo.com/"
-        one-link-mark="yes"
-        target="_blank"
-      >
-        <span id="hitokoto" />
-      </a>
-    </span>
-    <a
-      href="https://blog.nankoyo.com/"
-      one-link-mark="yes"
-      target="_blank"
-    >
-    </a>
-  </div>
-  <a
-    href="https://blog.nankoyo.com/"
-    one-link-mark="yes"
-    target="_blank"
-  >
-    <div className="github-badge-big">
-      <span className="badge-subject">
-        <i className="fa fas-clock-o" />
-        {' '}Running Time
-      </span>
-      <span className="badge-value bg-apricots">
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_days"
-        />
-        days
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_hours"
-        />
-        {' '}H
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_mins"
-        />
-        {' '}M
-        <span
-          className="odometer odometer-auto-theme"
-          id="blog_running_secs"
-        />
-      </span>
-    <span className='hidden busuanzi_container_site_pv'>
-    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
-    <span className='pl-2 hidden busuanzi_container_site_uv'>
-    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>                                                                 </div>
+            <div
+            id='footer-bottom'
+            className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+            <div id='footer-bottom-left'>     
+            Copyright{' '}<i className='mx-1 animate-pulse fas fa-registered' />2021-2024{' '}
+          <a
+            href='https://blog.nankoyo.com/'
+            className='underline font-bold dark:text-gray-300 '>
+           NanKoYo, Inc
+          </a>{' '}All Rights Reserved. <br /><br />
+          <i className='fa-solid fa-language' />{' '}
+              <a href='https://blog.nankoyo.com/' className='mr-2'>
+                一言: <span id="hitokoto"></span>
+          </a><br />
+         <i className='fas fa-shield-alt' />{' '}
+              <a href='https://www.cloudflare.com/' className='mr-2'>
+                当前CDN节点: <span id="cdn"></span>
+              </a>
         </div>
-        </footer >
+        
+          <span className='hidden busuanzi_container_site_pv'>
+            <i className='fas fa-eye' />
+            <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
+          </span>
+          <span className='pl-2 hidden busuanzi_container_site_uv'>
+            <i className='fas fa-users' />{' '}
+            <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
+          </span>
+
+          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
+        </div>
+      </div>
+    </footer>
   )
 }
 
 export default Footer
+  
