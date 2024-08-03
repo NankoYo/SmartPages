ff3bd496d7d4176dd301894bad958a0df14df3be
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-08-04 01:01:26 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -26,9 +26,18 @@ const Footer = ({ title }) => {
 
             {/* 底部页面信息 */}
   <div
-            id='footer-bottom'
-            className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-    <div id='footer-bottom-left'>
+        id='footer-bottom'
+        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
+        <div id='footer-bottom-left'>
+          smartpage {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
+          {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
+          <a
+            href={siteConfig('LINK')}
+            className='underline font-bold dark:text-gray-300 '>
+            {siteConfig('AUTHOR')}
+          </a>
+          .
+        </div>
   <div className="github-badge-big">
     <span className="badge-subject">
       <i className="fa fa-id-card" />
